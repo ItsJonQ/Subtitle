76d44de0f6d128ddd33a0c5eca8fed73900a9fb9
ItsJonQ <itsjonq@gmail.com>
2013-12-14 19:22:31 -0500

    Added title feature to log

--- subtitle.js
+++ subtitle.js
@@ -1,23 +1,30 @@
 (function(subtitle, undefined) {
 	'use strict';
 	subtitle.options = {
-		name: 'Subtitle Log'
+		name: 'Subtitle JS',
+		log: 'Log'
 	};
 
-	subtitle.write = function(message) {
+	subtitle.write = function(message, options) {
 		var self = subtitle;
 		var output = '';
 		output += '\n';
 		output += '==============================================' + '\n';
-		output += subtitle.options.name + '\n';
+		output += subtitle.options.name + ' ' + subtitle.options.log + '\n';
 		output += '==============================================' + '\n';
+		if(options !== undefined) {
+			if(options.title !== 'undefined') {
+				output += options.title + '\n';
+				output += '----------------------------------------------' + '\n';
+			}
+		}
 		output += '\n';
 		output += message + '\n';
 		output += '\n';
 		output += '==============================================' + '\n';
 		return output;
 	};
-	subtitle.output = function(subject) {
+	subtitle.output = function(subject, options) {
 		var message;
 		var messageType = (typeof subject);
 		switch(messageType) {
@@ -33,12 +40,19 @@
 				message = subject;
 				break;
 		}
-		console.log(subtitle.write(message));
+		console.log(subtitle.write(message, options));
 	};
 	subtitle.log = function() {
 		for(var i = 0, len = arguments.length; i < len; i++) {
 			var arg = arguments[i];
-			subtitle.output(arg);
+			var options;
+			
+			if(typeof arg === 'object') {
+				if(arg.title !== 'undefined') {
+					options = { title: arg.title };
+				}
+			}
+			subtitle.output(arg, options);
 		}
 	};
 
--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,11 @@
 (function() {
 	"use strict";
+	subtitle.options.name = 'New Name';
 	var obj = {
 		holy: 'moly'
 	};
 	var func = function() {
 		return true;
 	};
-	subtitle.log(func);
+	subtitle.log(func, { title: 'Return True Function' });
 })();
